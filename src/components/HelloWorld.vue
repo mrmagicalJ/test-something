17c883b48c012228905a360876694abbb641ad47
diexihua <1078787975@qq.com>
2019-02-19 23:51:50 +0800

    create new project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,47 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>prop</h2>
+    <p>propA: {{propA}}</p>
+    <p>propB: {{propB}}</p>
+    <p>propC: {{propC}}</p>
+    <p>computedTest: {{computedTest}}</p>
+    <input type="text" :value="modalValue" @input="modalValuePlus">
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+// vue属性装饰器
+import { Vue, Component, Prop, Model, Emit, Watch, Inject } from 'vue-property-decorator';
 
 @Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default class Hello extends Vue {
+  @Prop(Number) private propA!: number;
+  @Prop({default: 'default value'}) private propB!: string;
+  @Prop([String, Boolean]) private propC!: string | boolean;
+  @Inject('msg1') private msg123!: string;
+
+  @Model('change', {type: String}) private modalValue!: string;
+
+  @Emit('change')
+  private modalValuePlus(e: Event) {
+    return (e.target as HTMLInputElement).value;
+  }
+  @Watch('modalValue')
+  private onModalValueChange(newValue: string, oldValue: string) {
+    // console.log(newValue, oldValue);
+  }
+
+  get computedTest() {
+    return this.propA + '666';
+  }
+
+  private mounted() {
+    // console.log(this.msg123);
+  }
+
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld v-model="modelData" :propA="msg" propC="Welcome to Your Vue.js + TypeScript App"/>
+    {{modelData}}
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue, Prop, Provide } from 'vue-property-decorator';
+import HelloWorld from '../components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+
+export default class Home extends Vue {
+  private msg: number = 12;
+  private modelData: string = '123';
+
+  @Provide() private msg1 = this;
+}
+
 </script>

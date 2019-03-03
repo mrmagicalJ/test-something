99cbe47fe34095c514d86254e0fc6fddc35dec38
diexihua <1078787975@qq.com>
2019-03-04 00:25:11 +0800

    add overrideArray

--- src/mvvm-test.ts
+++ src/mvvm-test.ts
@@ -1,3 +1,5 @@
+const OAM = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+
 class MvvmObj {
   constructor(obj: object, callback: Function) {
     if (Object.prototype.toString.call(obj) !== '[object Object]') {
@@ -33,7 +35,42 @@ class MvvmObj {
       }
     })
   }
+
+  private overrideArrayProto(arr: []) {
+    const originalProto = Array.prototype;
+    const overrideProto = Object.create(Array.prototype);
+    let result: any;
+
+    OAM.forEach((key, index, array) => {
+      const method = OAM[index];
+      const THIS = this;
+      let oldArr = [];
+
+      Object.defineProperty(overrideProto, method, {
+        value() {
+          oldArr = this.slice(0);
+
+          // 将类数组转化为数组
+          let arg = [].slice.apply(arguments);
+          // 调用原始 原型 的数组方法
+          result = originalProto[method].apply(this, arg);
+          // 对新的数组进行监测
+          THIS.observe(this);
+          // 执行回调
+          THIS.callback(this, oldArr);
+
+          return result;
+        }
+      })
+
+      // 最后 让该数组实例的 __proto__ 属性指向 假的原型 overrideProto
+      // array.__proto__ = overrideProto;
+    })
+
+  }
+
 }
+
 
 let data = {
   a: 200,
@@ -54,3 +91,4 @@ const mv: MvvmObj = new MvvmObj(data, callback);
 data.a = 300;
 data.level1.b = 'sssss';
 data.level1.level2.d = 888;
+data.level1.c.push(4);
